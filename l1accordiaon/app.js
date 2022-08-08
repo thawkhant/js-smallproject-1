c8cea3b2e09af6daa9965592564ead0a92115168
thawkhant <thawkhant1290@gmail.com>
2022-08-08 10:48:32 +0630

    08 08 2022

---
+++ l1accordiaon/app.js
@@ -0,0 +1,36 @@
+var getacctitles = document.getElementsByClassName("acctitle");
+// console.log(getacctitles);   // HTML Collections
+var getacccontents = document.querySelectorAll(".acccontent");
+//console.log(getacccontents);  Node List
+
+for(var x = 0; x < getacctitles.length; x++){
+	// console.log(x);
+
+	getacctitles[x].addEventListener("click",function(e){
+		// console.log(e.target);
+		// console.log(this);
+
+		this.classList.toggle("active");
+		var getcontent = this.nextElementSibling;
+		// console.log(getcontent);
+
+
+		if(getcontent.style.height){
+			getcontent.style.height = null;  // care we can't set 0
+
+		}else{
+			// console.log(getcontent.scrollHeight);
+			getcontent.style.height = getcontent.scrollHeight + "px";
+		}
+	});
+
+	if(getacctitles[x].classList.contains("active")){
+		getacccontents[x].style.height = getacccontents[x].scrollHeight+"px";
+	}
+	
+
+}
+
+
+
+
